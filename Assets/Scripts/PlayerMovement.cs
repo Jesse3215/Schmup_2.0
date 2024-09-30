1cd3e599f5eedfceab66d24c41b0179504904d32
Jesse3215 <jessefaassen20073215@gmail.com>
2024-09-30 14:56:46 +0200

    working on input system

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float moveSpeed;
+
+    private Vector2 moveDirection;
+
+    public InputActionReference move;
+
+    private void Update()
+    {
+        moveDirection = move.action.ReadValue<Vector2>();
+    }
+}
