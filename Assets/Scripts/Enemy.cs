e4bffe925946a1e0c39c6d68f84acdfe73e6e1d9
Jesse3215 <jessefaassen20073215@gmail.com>
2024-10-14 14:32:45 +0200

    enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Vector3 leftPos;
+    public Vector3 rightPos;
+
+    public GameObject player;
+
+    private void Update()
+    {
+       
+    }
+}
