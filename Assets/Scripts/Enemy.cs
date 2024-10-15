220aa076dab83cd0700e2798e08411faeb3cdd86
Jesse3215 <jessefaassen20073215@gmail.com>
2024-10-15 18:03:34 +0200

    working enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,13 +4,49 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public Vector3 leftPos;
-    public Vector3 rightPos;
+    public float speed = 2f;
 
-    public GameObject player;
+    [SerializeField] private float rightpos;
+    [SerializeField] private float leftpos;
 
-    private void Update()
+    [SerializeField] private bool movingRight = true;
+    [SerializeField] private bool movingLeft = false;
+
+    void Start()
     {
-       
     }
+
+    void Update()
+    {
+        if (movingRight)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+            if (transform.position.x <= rightpos)
+            {
+                movingRight = false;
+                movingLeft = true;
+            }
+
+        }
+        if (movingLeft)
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+
+            if (transform.position.x >= leftpos)
+            {
+                movingLeft = false;
+                movingRight = true;
+            }
+
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
 }
