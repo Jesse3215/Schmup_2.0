036103bf13e592f285cccb89273ddedf32ef789e
Jesse3215 <jessefaassen20073215@gmail.com>
2024-10-18 09:16:03 +0200

    waveswaner fixed

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -8,14 +8,24 @@ public class Projectile : MonoBehaviour
 
     public int bulletSpeed;
 
+    private WaveSpwaner waveSpwaner;
+
     private void Start()
     {
+        waveSpwaner = FindAnyObjectByType<WaveSpwaner>();
+
         rb.AddForce(Vector2.up * bulletSpeed, ForceMode2D.Impulse);
         Destroy(gameObject, 3);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            waveSpwaner.enemiesSpawned.Remove(collision.gameObject);
+            Destroy(collision.gameObject);
+        }
+
         Destroy(gameObject);
     }
 
--- Assets/Scripts/WaveSpwaner.cs
+++ Assets/Scripts/WaveSpwaner.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 
 public class WaveSpwaner : MonoBehaviour
 {
+    [SerializeField] public List<GameObject> enemiesSpawned = new List<GameObject>();
+    public GameObject enemyClone;
+
     private float rightPos = 9.39f;
     private float leftPos = -9.39f;
 
     public TextMeshProUGUI waveCountText;
-    int waveCount = 1;
+    int waveCount = 0;
 
     public float spawnRate = 1.0f;
     public float timeBetweenWaves = 3.0f;
@@ -24,30 +27,36 @@ public class WaveSpwaner : MonoBehaviour
     {
         waveCountText.text = "Wave: " + waveCount.ToString();
 
-        if (waveIsDone == true)
+        /*if (waveIsDone == true)
         {
             StartCoroutine(waveSpawner());
+        }*/
+
+        if(enemiesSpawned.Count <= 0) 
+        {
+            waveCount += 1;
+            StartCoroutine(waveSpawner());
         }
+
     }
 
     IEnumerator waveSpawner()
     {
-        waveIsDone = false;
 
         for (int i = 0; i < enemyCount; i++)
         {
-            GameObject enemyClone = Instantiate(enemy, transform.position + Vector3.right * Random.Range(leftPos, rightPos), Quaternion.Euler(new Vector3(0, 0, 180)));
+            enemyClone = Instantiate(enemy, transform.position + Vector3.right * Random.Range(leftPos, rightPos), Quaternion.Euler(new Vector3(0, 0, 180)));
+
+            enemiesSpawned.Add(enemyClone);
 
             yield return new WaitForSeconds(spawnRate);
         }
 
         //spawnRate -= 0.1f;
-       enemyCount += 1;
-        waveCount += 1;
+        enemyCount += 1;
 
         yield return new WaitForSeconds(timeBetweenWaves);
 
-        waveIsDone = true;
     }
 }
 
